4f6b85347b6e291dfd7cb9951dbaa09b95678293
6im0n <simon.gl3135@orange.fr>
2024-10-02 04:15:53 +0200

    :white_check_mark: Add test for the server

--- tests/server/main.cpp
+++ tests/server/main.cpp
@@ -6,10 +6,33 @@
 ** You can even have multiple lines if you want !
 */
 
-#include "../src/network/server/server.hpp"
+#include "server/Server.hpp"
+#include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    hello_world();
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <TCP_port> <UDP_port>" << std::endl;
+        return 1;
+    }
+
+    const unsigned short TCP_port = static_cast<unsigned short>(std::stoi(argv[1]));
+    const unsigned short UDP_port = static_cast<unsigned short>(std::stoi(argv[2]));
+
+    try {
+        Network::Server server(TCP_port, UDP_port);
+        server.start([](const std::vector<uint8_t>& data, const asio::ip::udp::endpoint& client_endpoint) {
+            // Handle received data
+            std::cout << "Received data from client: " << client_endpoint << std::endl;
+        });
+
+
+
+        std::cout << "Server started on TCP port " << TCP_port << " and UDP port " << UDP_port << std::endl;
+
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
     return 0;
 }
