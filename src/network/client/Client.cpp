4995d9b45c273877c85a5886cb56d17d52ce0a42
6im0n <simon.gl3135@orange.fr>
2024-10-02 03:49:48 +0200

    :sparkles: Add the network client architecture and implementation

--- src/network/client/Client.cpp
+++ src/network/client/Client.cpp
@@ -5,34 +5,79 @@
 ** No file there , just an epitech header example .
 ** You can even have multiple lines if you want !
 */
-
 #include "Client.hpp"
-#include <asio/ip/address.hpp>
+#include <iostream>
+#include <utility>
+#include <asio/error_code.hpp>
 #include <asio/ip/udp.hpp>
 
-Client::Client(const std::string& host, unsigned short port)
-    : _socket(_io_context), _endpoint(asio::ip::make_address(host), port) {
-    _socket.open(asio::ip::udp::v4());
+Network::Client::Client(std::string host, const unsigned short udp_port, unsigned short tcp_port)
+    : _host(std::move(host)), _UDP_PORT(udp_port), _TCP_PORT(tcp_port), _udp_socket(_io_context), _tcp_socket(_io_context)
+{
+    _id = -1;
 }
 
-Client::~Client() {
-    disconnect();
+Network::Client::~Client()
+{
+    _io_context.stop();
 }
 
-void Client::send(const std::vector<uint8_t>& data) {
-    _socket.send_to(asio::buffer(data), _endpoint);
-}
+void Network::Client::connect(callback function)
+{
+    // TCP connection init
+    asio::ip::tcp::resolver resolver(_io_context);
+    asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(_host, std::to_string(_TCP_PORT));
 
-std::vector<uint8_t> Client::receive() {
-    std::vector<uint8_t> buffer(1024);
-    asio::ip::udp::endpoint sender_endpoint;
-    size_t length = _socket.receive_from(asio::buffer(buffer), sender_endpoint);
-    buffer.resize(length);
-    return buffer;
-}
-
-void Client::disconnect() {
-    if (_socket.is_open()) {
-        _socket.close();
+    asio::error_code error;
+    asio::connect(_tcp_socket, endpoints, error);
+    if (error) {
+        std::cerr << "Error: " << error.message() << std::endl;
+        return;
     }
+
+    asio::read(_tcp_socket, asio::buffer(&_id, sizeof(_id)), error);
+    if (error) {
+        std::cerr << "Error: " << error.message() << std::endl;
+        return;
+    } else {
+        std::cout << "Connected to server with id " << static_cast<int>(_id) << std::endl;
+    }
+
+    // UDP connection init
+    _udp_socket.open(asio::ip::udp::v4());
+    _endpoint = asio::ip::udp::endpoint(asio::ip::address::from_string(_host), _UDP_PORT);
+    _callback = std::move(function);
+    receive();
+}
+
+void Network::Client::send(const std::vector<uint8_t> &data)
+{
+    _udp_socket.async_send_to(asio::buffer(data), _endpoint,
+        [](const asio::error_code &error, std::size_t bytes_transferred) {
+            if (error) {
+                std::cerr << "Error: " << error.message() << std::endl;
+            }
+        });
+    std::cout << "Data sent" << std::endl;
+}
+
+void Network::Client::receive() {
+    _udp_socket.async_receive_from(asio::buffer(_recv_buffer), _endpoint,
+        [this](const asio::error_code &error, std::size_t bytes_read)
+        {
+            if (!error) {
+                if(_callback) {
+                    _callback(_recv_buffer, _endpoint);
+                }
+                receive();
+            } else {
+                std::cerr << "Error: " << error.message() << std::endl;
+            }
+        });
+}
+
+void Network::Client::disconnect()
+{
+    _tcp_socket.close();
+    _udp_socket.close();
 }
--- src/network/client/Client.hpp
+++ src/network/client/Client.hpp
@@ -12,20 +12,95 @@
 #include <asio.hpp>
 #include <vector>
 
-class Client {
-public:
-    Client(const std::string& host, unsigned short port);
-    ~Client();
+/**
+ * @namespace Network
+ * @brief Main namespace for the network library
+ * @details This namespace contains all the classes, functions and enums for the network library
+ * @version 0.1.0
+ * @since 0.1.0
+ * @author Simon GANIER-LOMBARD
+ */
+namespace Network {
 
-    bool isConnected();
-    void send(const std::vector<uint8_t>& data);
-    std::vector<uint8_t> receive();
-    void disconnect();
+    /**
+     * @class Client
+     * @brief Represents a network client
+     * @details This class handles the network client operations such as connecting, sending, receiving, and disconnecting.
+     * @version 0.1.0
+     * @since 0.1.0
+     * @author Simon GANIER-LOMBARD
+     */
+    class Client {
 
-private:
-    asio::io_context _io_context;
-    asio::ip::udp::socket _socket;
-    asio::ip::udp::endpoint _endpoint;
-};
+    using callback = std::function<void(const std::vector<uint8_t> &data, const asio::ip::udp::endpoint &client_endpoint)>;
+    public:
+        /**
+         * @brief Constructs a new network client object
+         * @param host The host address to connect to
+         * @param udp_port The UDP port to use
+         * @param tcp_port The TCP port to use
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        Client(std::string host, unsigned short udp_port, unsigned short tcp_port);
+
+        /**
+         * @brief Destroys the network client object
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        ~Client();
+
+        /**
+         * @brief Connects to the server with tcp and udp sockets
+         * @param function The callback function to handle received data
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        void connect(callback function);
+
+        /**
+         * @brief Sends data to the server with udp socket
+         * @param data The data to send
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        void send(const std::vector<uint8_t> &data);
+
+        /**
+         * @brief Receives data from the server with udp socket
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        void receive();
+
+        /**
+         * @brief Disconnects from the client from the server
+         * @version 0.1.0
+         * @since 0.1.0
+         * @author Simon GANIER-LOMBARD
+         */
+        void disconnect();
+
+    private:
+        std::string _host; ///< The host address
+        unsigned short _TCP_PORT; ///< The TCP port
+        unsigned short _UDP_PORT; ///< The UDP port
+
+        asio::io_context _io_context; ///< The IO context
+        asio::ip::udp::socket _udp_socket; ///< The UDP socket
+        asio::ip::tcp::socket _tcp_socket; ///< The TCP socket
+        asio::ip::udp::endpoint _endpoint; ///< The UDP endpoint
+
+        int8_t _id; ///< The client ID
+        std::vector<uint8_t> _recv_buffer; ///< The receive buffer
+        callback _callback; ///< The callback function
+    };
+}
 
 #endif // CLIENT_HPP
