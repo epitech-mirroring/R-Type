c3d20024cac8beeb2db63ea5ec7d583aae29040d
6im0n <simon.gl3135@orange.fr>
2024-10-02 04:15:47 +0200

    :white_check_mark: Add test for the client

--- tests/client/main.cpp
+++ tests/client/main.cpp
@@ -6,10 +6,45 @@
 ** You can even have multiple lines if you want !
 */
 
-#include "../src/network/client/client.hpp"
+#include "client/Client.hpp"
 
-int main()
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#include "client/Client.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[])
 {
-    hello_world();
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <host> <ports>" << std::endl;
+        return 1;
+    }
+
+    const std::string host = argv[1];
+    const unsigned short TCP_port = static_cast<unsigned short>(std::stoi(argv[2]));
+    const unsigned short UDP_port = static_cast<unsigned short>(std::stoi(argv[3]));
+
+    try {
+        Network::Client client(host, UDP_port, TCP_port);
+        std::cout << "Client trying to connect to server..." << std::endl;
+        client.connect([](const std::vector<uint8_t>& data, const asio::ip::udp::endpoint& client_endpoint) {
+            // Handle received data
+        });
+
+        const std::vector<uint8_t> message = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
+        client.send(message);
+        sleep(6);
+
+        std::cout << "Message sent: " << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
     return 0;
 }
