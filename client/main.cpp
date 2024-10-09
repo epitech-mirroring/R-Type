453895223f3803cf4395d2508a9061fb8eb917ea
Marius-P1 <marius.pain@epitech.eu>
2024-10-09 15:36:57 +0200

    :sparkles: Add client  main.cpp file

---
+++ client/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is the client" << std::endl;
+    return 0;
+}
