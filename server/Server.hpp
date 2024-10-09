810eed47768cf8fe26ea62a931a2a140d6e9098e
Marius-P1 <marius.pain@epitech.eu>
2024-10-09 15:43:36 +0200

    :sparkles: Add Server class for server logic

---
+++ server/Server.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** Server.hpp
+*/
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+class Server {
+	//TODO : Add server logic
+};
+
+
+
+#endif //SERVER_HPP
