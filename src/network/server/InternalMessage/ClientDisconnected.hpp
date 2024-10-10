1060fa4eb932e517111bb2dd78a0ea0a7dac1aa4
6im0n <simon.gl3135@orange.fr>
2024-10-10 09:27:25 +0200

    :sparkles: Add message interface for server interaction

---
+++ src/network/server/InternalMessage/ClientDisconnected.hpp
@@ -0,0 +1,63 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#ifndef CLIENTDISCONNECTED_HPP
+#define CLIENTDISCONNECTED_HPP
+
+#include <utility>
+#include "IInternalMessageClient.hpp"
+
+/**
+ * @class ClientDisconnected
+ * @brief Represents a client disconnection message
+ * @details This class handles the client disconnection message, including the message content and client ID.
+ * @version 0.1.0
+ * @since 0.1.0
+ * @author Simon GANIER-LOMBARD
+ */
+class ClientDisconnected final : public IInternalMessageClient {
+public:
+ /**
+  * @brief Constructor
+  * @param message The disconnection message
+  * @param id The client ID
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+ ClientDisconnected(std::string message, std::string id) : _message(std::move(message)), _id(std::move(id)) {}
+
+ /**
+  * @brief Destructor
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+ ~ClientDisconnected() override = default;
+
+ /**
+  * @brief Gets the client ID
+  * @return The client ID
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+ [[nodiscard]] int getId() const override;
+
+ /**
+  * @brief Gets the disconnection message
+  * @return The disconnection message
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+ [[nodiscard]] std::string getMessage() const override;
+
+private:
+ std::string _message; ///< The disconnection message
+ std::string _id; ///< The client ID
+ std::chrono::time_point<std::chrono::system_clock> _time; ///< The time of the disconnection
+};
+
+#endif // CLIENTDISCONNECTED_HPP
