1f90cca23b1eff0ecba5806e5ee02d38b62ae14b
Aubane <aubane.nourry@epitech.eu>
2024-10-11 23:04:36 +0200

    :sparkles: Add background cpp monobehaviour

--- client/objects/scripts/Background.cpp
+++ client/objects/scripts/Background.cpp
@@ -5,14 +5,36 @@
 ** Background.cpp
 */
 
-void before() {
+#include <ctime>
+#include "StellarForge/common/components/CPPMonoBehaviour.hpp"
+#include "StellarForge/common/components/Transform.hpp"
 
-}
+class Background : public CPPMonoBehaviour {
+    public:
+        Background() {
+            start = clock();
+        }
+        ~Background() = default;
 
-void update() {
-
-}
-
-void after() {
-
-}
+        Transform *getOwnerTransform() {
+            for (auto &component : this->getOwner()->getComponents()) {
+                if (component->getMeta().getName() == "Transform") {
+                    return dynamic_cast<Transform *>(component);
+                }
+            }
+            return nullptr;
+        }
+        void update() override {
+            clock_t actual = clock();
+            if ((actual - start) / CLOCKS_PER_SEC >= 0.10f / speed) {
+                start = actual;
+                auto *transform = getOwnerTransform();
+                transform->setPosition(transform->getPosition().x - 1, transform->getPosition().y);
+                if (transform->getPosition().x <= -1920)
+                    transform->setPosition(0, transform->getPosition().y);
+            }
+        }
+    private:
+        float speed = 1.00f;
+        clock_t start;
+};
