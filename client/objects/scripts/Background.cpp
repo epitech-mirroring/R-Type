3cff52ba175aad1f1be808645e02e93ade3850f1
Aubane <aubane.nourry@epitech.eu>
2024-10-11 19:53:46 +0200

    :construction: Add empty cpp behaviours

---
+++ client/objects/scripts/Background.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** Background.cpp
+*/
+
+void before() {
+
+}
+
+void update() {
+
+}
+
+void after() {
+
+}
---
+++ client/objects/scripts/NetworkManager.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** NetworkManager.cpp
+*/
+
+void before() {
+
+}
+
+void update() {
+
+}
+
+void after() {
+
+}
