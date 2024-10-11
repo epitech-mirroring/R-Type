27ccd44d92fa3cd727f3e820a3998a5a6163b249
landryarki <landry.gigant@epitech.eu>
2024-10-11 18:14:24 +0200

    :sparkles: add Binary Vectors
    to convert data to vector of char for better data transfering

---
+++ server/utils/BinaryVector.cpp
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** No file there , just an epitech header example .
+*/
+
+#include "BinaryVector.hpp"
+
+BinaryVector::BinaryVector(const std::vector<char> &vector) : _vector(vector)
+{
+}
+
+BinaryVector &BinaryVector::pushString(const std::string& value)
+{
+    for (char const chr : value) {
+        _vector.push_back(chr);
+    }
+    _vector.push_back('\0');
+    return *this;
+}
+
+std::string BinaryVector::consumeString()
+{
+    std::string value;
+
+    while (_vector[0] != '\0') {
+        value += _vector[0];
+        _vector.erase(_vector.begin());
+    }
+    _vector.erase(_vector.begin());
+    return value;
+}
+
+BinaryVector &BinaryVector::operator=(const std::vector<char> &vector)
+{
+    _vector = vector;
+    return *this;
+}
+
+BinaryVector::operator std::vector<char>() const
+{
+    return _vector;
+}
+
+// BinaryOperation
+
+std::vector<char> BinaryConversion::convertString(const std::string& value)
+{
+    std::vector<char> _vector;
+
+    for (char const chr : value) {
+        _vector.push_back(chr);
+    }
+    _vector.push_back('\0');
+    return _vector;
+}
+
+std::string BinaryConversion::consumeString(std::vector<char> &_vector)
+{
+    std::string value;
+
+    while (_vector[0] != '\0') {
+        value += _vector[0];
+        _vector.erase(_vector.begin());
+    }
+    _vector.erase(_vector.begin());
+    return value;
+}
+
+std::vector<char> operator+(std::vector<char> &&vec_a, std::vector<char> &&vec_b) {
+    std::vector<char> vec_c = vec_a;
+    vec_c.insert(vec_c.end(), vec_b.begin(), vec_b.end());
+    return vec_c;
+}
+
+std::vector<char> operator+=(std::vector<char> &vec_a, std::vector<char> &&vec_b) {
+    vec_a.insert(vec_a.end(), vec_b.begin(), vec_b.end());
+    return vec_a;
+}
---
+++ server/utils/BinaryVector.hpp
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type-Reborn
+** File description:
+** No file there , just an epitech header example .
+*/
+
+#ifndef R_TYPE_REBORN_BINARYOPERATION_HPP
+#define R_TYPE_REBORN_BINARYOPERATION_HPP
+
+#include <vector>
+#include <string>
+
+class BinaryVector {
+public:
+    BinaryVector() = default;
+    BinaryVector(const std::vector<char> &vector);
+    ~BinaryVector() = default;
+
+    BinaryVector &pushString(const std::string& value);
+
+    template<class T>
+    BinaryVector &push(T value)
+    {
+        char *ptr = reinterpret_cast<char *>(&value);
+
+        for (int i = 0; i < sizeof(T); i++)
+            _vector.push_back(ptr[i]);
+        return *this;
+    }
+
+    std::string consumeString();
+
+    template<class T>
+    T consume()
+    {
+        T value;
+        char *ptr = reinterpret_cast<char *>(&value);
+
+        for (int i = 0; i < sizeof(T); i++)
+            ptr[i] = _vector[i];
+        _vector.erase(_vector.begin(), _vector.begin() + sizeof(T));
+        return value;
+    }
+
+    BinaryVector &operator=(const std::vector<char> &vector);
+
+    operator std::vector<char>() const;
+
+private:
+    std::vector<char> _vector;
+};
+
+namespace BinaryConversion {
+
+    std::vector<char> convertString(const std::string& value);
+
+    template<class T>
+    std::vector<char> convert(T value)
+    {
+        std::vector<char> _vector;
+        char *ptr = reinterpret_cast<char *>(&value);
+
+        for (int i = 0; i < sizeof(T); i++)
+            _vector.push_back(ptr[i]);
+        return _vector;
+    }
+
+    std::string consumeString(std::vector<char> &_vector);
+
+    template<class T>
+    T consume(std::vector<char> &_vector)
+    {
+        T value;
+        char *ptr = reinterpret_cast<char *>(&value);
+
+        for (int i = 0; i < sizeof(T); i++)
+            ptr[i] = _vector[i];
+        _vector.erase(_vector.begin(), _vector.begin() + sizeof(T));
+        return value;
+    }
+}
+
+std::vector<char> operator+(std::vector<char> &&a, std::vector<char> &&b);
+std::vector<char> operator+=(std::vector<char> &a, std::vector<char> &&b);
+
+
+#endif //R_TYPE_REBORN_BINARYOPERATION_HPP
