d6cb732fa3065af6872cfb9fb2348b33dd51e31b
6im0n <simon.gl3135@orange.fr>
2024-10-12 17:30:21 +0200

    :pencil2: Bad indentation

--- network/client/ISessionClient.hpp
+++ network/client/ISessionClient.hpp
@@ -41,82 +41,80 @@ namespace Network {
    */
   virtual void connect() = 0;
 
-  /**
+   /**
    * @brief Sends data to the server
    * @param data The data to send
    * @version 0.1.0
    * @since 0.1.0
    */
-  virtual void send_udp_data(const std::vector<char> &data) = 0;
+   virtual void send_udp_data(const std::vector<char> &data) = 0;
 
-  /**
+   /**
    * @brief receive data from the server
    * @version 0.1.0
    * @since 0.1.0
    */
-  virtual void receive_udp_data() = 0;
+   virtual void receive_udp_data() = 0;
 
-     /**
-    * @brief Receives data from the server with tcp socket
-    * @version 0.1.0
-    * @since 0.1.0
-    * @author Simon GANIER-LOMBARD
-    */
-     virtual void receive_tcp_data() = 0 ;
-     /**
-      *
-     * @brief Sends data to the server with tcp socket
-     * @param data The data to send
-     * @version 0.1.0
-     * @since 0.1.0
-     * @author Simon GANIER-LOMBARD
-     */
-     virtual void send_tcp_data(const std::string& data) = 0;
+   /**
+   * @brief Receives data from the server with tcp socket
+   * @version 0.1.0
+   * @since 0.1.0
+   * @author Simon GANIER-LOMBARD
+   */
+   virtual void receive_tcp_data() = 0 ;
+   /**
+   *
+   * @brief Sends data to the server with tcp socket
+   * @param data The data to send
+   * @version 0.1.0
+   * @since 0.1.0
+   * @author Simon GANIER-LOMBARD
+   */
+   virtual void send_tcp_data(const std::string& data) = 0;
 
-     /**
-      * @brief Sends data to a client with a specific ID in UDP mode
-      * @param data The data to send
-      * @param id The ID of the client to send data to
-      * @version 0.1.0
-      * @since 0.1.0
-      * @author Simon GANIER-LOMBARD
-      */
-     virtual void add_to_send_queue(const std::vector<char> &data) = 0;
-
-
-     /**
-     * @brief Get the next receive queue data
-     * @return The receive queue data
-     * @version 0.1.0
-     * @since 0.1.0
-     */
-      virtual std::vector<char> get_next_recv_queue() = 0;
-
-      /**
-       * @brief Get the size of the receive queue
-       * @return The size of the receive queue
-       * @version 0.1.0
-       * @since 0.1.0
-       */
-     virtual std::uint8_t get_size_recv_queue() = 0 ;
+   /**
+   * @brief Sends data to a client with a specific ID in UDP mode
+   * @param data The data to send
+   * @param id The ID of the client to send data to
+   * @version 0.1.0
+   * @since 0.1.0
+   * @author Simon GANIER-LOMBARD
+   */
+   virtual void add_to_send_queue(const std::vector<char> &data) = 0;
 
 
+   /**
+   * @brief Get the next receive queue data
+   * @return The receive queue data
+   * @version 0.1.0
+   * @since 0.1.0
+   * @author Simon GANIER-LOMBARD
+   */
+   virtual std::vector<char> get_next_recv_queue() = 0;
 
-  /**
+   /**
+   * @brief Get the size of the receive queue
+   * @return The size of the receive queue
+   * @version 0.1.0
+   * @since 0.1.0
+   * @author Simon GANIER-LOMBARD
+   */
+   virtual std::uint8_t get_size_recv_queue() = 0 ;
+
+   /**
    * @brief Stops the client and closes all connections
    * @version 0.1.0
    * @since 0.1.0
    */
-  virtual void stop() = 0;
+   virtual void stop() = 0;
 
-
-
-  /**
+   /**
    * @brief Destructor
    * @version 0.1.0
    * @since 0.1.0
    */
-  virtual ~ISessionClient() = default;
+   virtual ~ISessionClient() = default;
  };
 }
 
