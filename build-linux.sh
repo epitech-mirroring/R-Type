4ef7d20daf334a09367d4b3589138f53a944826a
6im0n <simon.gl3135@orange.fr>
2024-10-23 03:26:32 +0200

    :building_construction: Add multythreading to the script

--- build-linux.sh
+++ build-linux.sh
@@ -44,10 +44,13 @@ conan install .. --build=missing --profile="$CONAN_PROFILE" -g CMakeDeps -g CMak
 info "Running CMake to configure the build..."
 cmake -S .. -B . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_PREFIX_PATH="." -DCMAKE_BUILD_TYPE=Release
 
-# Step 5: Build the project with CMake
-info "Building the project with CMake..."
-cmake --build . --target r-type_server
+# Step 5: Detect number of available threads for parallel build
+NUM_THREADS=$(nproc --all)
+info "Number of threads available for build: $NUM_THREADS"
 
-# Step 5: Build the project with CMake
-info "Building the project with CMake..."
-cmake --build . --target r-type_client
+# Step 6: Build the project with CMake
+info "Building the server project with CMake..."
+cmake --build . --target r-type_server -- -j "$NUM_THREADS"
+
+info "Building the client project with CMake..."
+cmake --build . --target r-type_client -- -j "$NUM_THREADS"
