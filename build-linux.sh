8dbc2e63febdf021f6ca79980c4eabea3d30f00e
6im0n <simon.gl3135@orange.fr>
2024-11-02 23:59:25 +0100

    :building_construction: Add the detection of OS type

--- build-linux.sh
+++ build-linux.sh
@@ -7,12 +7,47 @@ set -e
 BUILD_DIR="build"
 CONAN_PROFILE="default"
 
+#Get the os type
+os=$(uname -a)
+
+#Choose the package manager based on the os type
+if [[ $os == *"Ubuntu"* ]]; then
+  package_manager="apt-get"
+elif [[ $os == *"Fedora"* ]]; then
+  package_manager="dnf"
+elif [[ $os == *"CentOS"* ]]; then
+  package_manager="yum"
+elif [[ $os == *"Darwin"* ]]; then
+  package_manager="brew"
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
 # Function to print info messages in yellow color
 info() {
   echo -e "\033[1;33m[info] $1\033[0m"
 }
 
-# Step 1: Install Conan if not already installed
+# Step: Check if python is installed
+info "Checking if python is installed..."
+if ! command -v python &> /dev/null
+then
+    info "Python not found, installing Python..."
+    sudo $package_manager install -y python
+fi
+info "Python is installed."
+
+# Step: Check if pip is installed
+info "Checking if pip is installed..."
+if ! command -v pip &> /dev/null
+then
+    info "Pip not found, installing Pip..."
+    sudo $package_manager install -y python-pip
+fi
+info "Pip is installed."
+
+# Install Conan if not already installed
 info "Checking if Conan is installed..."
 if ! command -v conan &> /dev/null
 then
@@ -20,14 +55,39 @@ then
     pip install conan
 fi
 
-# Step 2: Create build directory if it does not exist
+# Install CMake if not already installed
+info "Checking if CMake is installed..."
+if ! command -v cmake &> /dev/null
+then
+    info "CMake not found, installing CMake..."
+    sudo $package_manager install -y cmake
+fi
+
+# Install Ninja if not already installed
+info "Checking if Ninja is installed..."
+if ! command -v ninja &> /dev/null
+then
+    info "Ninja not found, installing Ninja..."
+    sudo $package_manager install -y ninja-build
+fi
+
+# Install C++ compiler if not already installed
+info "Checking if C++ compiler is installed..."
+if ! command -v g++ &> /dev/null
+then
+    info "C++ compiler not found, installing C++ compiler..."
+    sudo $package_manager install -y gcc-c++
+fi
+
+# Step: Create build directory if it does not exist
 info "Creating build directory if it does not exist..."
 if [ ! -d "$BUILD_DIR" ]; then
   info "Creating build directory..."
   mkdir -p "$BUILD_DIR"
 fi
 
-# Step 3: Run Conan to install missing dependencies
+
+# Step Run Conan to install missing dependencies
 info "Changing to build directory..."
 cd "$BUILD_DIR"
 
@@ -43,15 +103,15 @@ conan remote add Epitech-Mirroring https://nexus.place2die.com/repository/Epitec
 info "Installing dependencies with Conan..."
 conan install .. --build=missing --profile="$CONAN_PROFILE" -g CMakeDeps -g CMakeToolchain
 
-# Step 4: Run CMake to configure the build
+# Step: Run CMake to configure the build
 info "Running CMake to configure the build..."
 cmake -S .. -B . -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_PREFIX_PATH="." -DCMAKE_BUILD_TYPE=Release
 
-# Step 5: Detect number of available threads for parallel build
+# Step: Detect number of available threads for parallel build
 NUM_THREADS=$(nproc --all)
 info "Number of threads available for build: $NUM_THREADS"
 
-# Step 6: Build the project with CMake
+# Step: Build the project with CMake
 info "Building the server project with CMake..."
 cmake --build . --target r-type_server -- -j "$NUM_THREADS"
 
